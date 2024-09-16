14f8180682c01fe45d8d6596f22214ba87556d64
Shoyaishida98 <svaria6@gmail.com>
2024-09-16 22:11:32 +0530

    Add files via upload

---
+++ assignment ques 12/js/slider.js
@@ -0,0 +1,20 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showSlide(slideIndex) {
+    slides.forEach((slide, index) => {
+        if (index === slideIndex) {
+            slide.style.display = 'block';
+        } else {
+            slide.style.display = 'none';
+        }
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000); // Change slide every 3 seconds
+showSlide(currentSlide); // Show the first slide
